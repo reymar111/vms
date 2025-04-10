4675592bb2907856625f76a4c158d9ae10c68201
reymar panoy <amazing.reymar131@gmail.com>
2025-04-10 11:17:58 +0800

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
@@ -5,7 +6,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
@@ -17,4 +18,28 @@ export default defineConfig({
             },
         }),
     ],
+
+    define: {
+        'process.env': {},
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+    build: {
+        // outDir: 'public/build', // Ensure the output directory is correct
+        // emptyOutDir: true,
+        // manifest: true,
+        rollupOptions: {
+            external: [
+                '/logo.png',
+            ],
+            output: {
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+            },
+        },
+    },
 });
